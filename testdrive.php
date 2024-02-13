076f7e833775bb35ca767b60eea71e60571603e2
POORYAZARE <pooryazare.2019@gmail.com>
2024-02-13 13:19:11 +0200

    Update testdrive.php

--- testdrive.php
+++ testdrive.php
@@ -4,19 +4,24 @@
 <form name="testDriveForm" method="post" action="process.php">
     <div class="form-group">
         <label for="FirstName">First Name:</label>
-        <input type="text" class="form-control" id="FirstName" placeholder="Enter your first name" name="FirstName" required>
+        <input type="text" class="form-control" id="FirstName" placeholder="Enter your first name" name="FirstName" required minlength="3" maxlength="30">
+        <span id = "nameError"></span>
     </div>
+
     <div class="form-group">
         <label for="LastName">Last Name:</label>
         <input type="text" class="form-control" id="LastName" placeholder="Enter your last name" name="LastName" required>
+        <span id = "nameError"></span>
     </div>
     <div class="form-group">
         <label for="Email">Email:</label>
         <input type="email" class="form-control" id="Email" placeholder="Enter your email" name="Email" required>
+        <span id = "emailError"></span>
     </div>
     <div class="form-group">
         <label for="CarID">Car ID:</label>
         <input type="number" class="form-control" id="CarID" placeholder="Enter the Car ID" name="CarID" required>
+        <span id = "caridError"></span>
     </div>
     <div class="form-group">
         <label for="Date">Test Drive Date:</label>
@@ -24,5 +29,36 @@
     </div>
     <button type="submit" class="btn btn-primary" name="submitTestDrive">Submit</button>
 </form>
+<script>
+    // function for name 
+    function validateName() {
+    const name= document.getElementById("name").valu;
+    const nameEroor= document.getElementById("nameEroor")
+
+    if(name.length < 3 || name.length > 20){
+        nameError.innerHtml = "Name must be between 3 & 20 characters";
+        return false ;
+    }
+    else{
+        nameError.innerHTML = "";
+        return true;
+    }
+    }
+
+        // function for email 
+        function validateEmail() {
+            const name= document.getElementById("email").valu;
+
+    }
+            // function for carid 
+            function validateCarID() {
+                const name= document.getElementById("carid").valu;
+
+
+    }
+// event listeners for real time validation
+document.getElementById("name").addEventListener("input", )
+</script>
+
 
 <?php include 'footer.php'; ?>
