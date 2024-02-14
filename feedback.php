587d1551d6a14c9b0d863c7bd18165abccc9a908
charity <charity23000@student.hamk.fi>
2024-02-14 21:31:42 +0200

    validation

--- feedback.php
+++ feedback.php
@@ -3,15 +3,15 @@ $title="Feedback";
 include 'header.php'; ?>
 
 <h2>Submit Your Feedback:</h2>
-<form name="feedbackForm" method="post" action="feedbackProcess.php"> 
+<form name="feedbackForm" method="post" action="feedbackProcess.php" required minlength="3" maxlength="20"> 
 
     <div class="form-group">
         <label for="Title">Title:</label>
-        <input type="text" class="form-control" id="Title" placeholder="Enter a title for your feedback" name="Title" required>
+        <input type="text" class="form-control" id="Title" placeholder="Enter a title for your feedback" name="Title"  required minlength="3" maxlength="20">
     </div>
     <div class="form-group">
         <label for="Comment">Comment:</label>
-        <textarea class="form-control" id="Comment" placeholder="Enter your feedback" name="Comment" required rows="4"></textarea>
+        <textarea class="form-control" id="Comment" placeholder="Enter your feedback" name="Comment" required rows="4"required minlength="3" maxlength="100"></textarea>
     </div>
     <div class="form-group">
         <label for="Rating">Rating:</label>
@@ -30,5 +30,63 @@ include 'header.php'; ?>
     </div>
     <button type="submit" class="btn btn-primary" name="submitFeedback">Submit Feedback</button>
 </form>
+
+<script>
+// function to validate firstname
+function validateName (){
+const Name= document.getElementById("Name").value;
+const NameError= document.getElementById ("NameError");
+if(Name.length < 3 || FirstName.length > 20 ){
+
+    NameError.innerHTML= "Name must be between 3 & 20 characters";
+    return false ; 
+}
+else {
+    NameError.innerHTML=""; 
+    return true;
+}
+
+}  
+
+    
+
+// function to validate email
+function validateEmail (){
+    const Email= document.getElementById("Email").value;
+    const emailError= document.getElementById ("emailError");
+
+if( Email === "" || !Email.includes("@") ){
+    emailError.innerHTML= "Please Enter a Valid Email Address";
+return false ; 
+}
+else {
+    emailError.innerHTML=""; 
+return true;
+}
+
+}
+// function to validate PhoneNumber
+function validatePhoneNumber (){
+    const PhoneNumber= document.getElementById("PhoneNumber").value;
+    const phoneError= document.getElementById ("phoneError");
+if(PhoneNumber.length < 3 || PhoneNumber.length > 20 ){
+
+    phoneError.innerHTML= "Phone Number must be between 8 & 20 number";
+return false ; 
+}
+else {
+    phoneError.innerHTML=""; 
+return true;
+}
+
+}
+
+// event listeners for real time validation
+document.getElementById("FirstName").addEventListener("input", validateFname);
+document.getElementById("LastName").addEventListener("input", validateLname);
+document.getElementById("Email").addEventListener("input", validateEmail);
+document.getElementById("PhoneNumber").addEventListener("input", validatePhoneNumber);
+
+</script>
 
 <?php include 'footer.php'; ?>
