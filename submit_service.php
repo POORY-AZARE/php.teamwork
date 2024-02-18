a5677c59d29ab9957851ea62fcc2ed9a7a7dd054
nastarandehnavi <nastaran23000@student.hamk.fi>
2024-02-18 07:01:54 +0200

    service task

--- submit_service.php
+++ submit_service.php
@@ -1,11 +1,11 @@
 <?php
 if (isset($_POST['ServiceRequest'])) {
     // Retrieve data from the form and store it in variables
-    $FirstName = $_POST['first_name'];     
-    $LastName = $_POST['last_name'];     
-    $Email = $_POST['email'];       
-    $PhoneNumber = $_POST['contact_number']; 
-    $CarType = $_POST['car_type'];
+    $first_name = $_POST['first_name'];     
+    $last_name = $_POST['last_name'];     
+    $email = $_POST['email'];       
+    $contact_number = $_POST['contact_number']; 
+    $car_type = $_POST['car_type'];
 
     // Include the database connection file
     include 'pz.php';
@@ -17,7 +17,7 @@ if (isset($_POST['ServiceRequest'])) {
     // Prepare the SQL statement to prevent SQL injection
     if ($stmt = $conn->prepare($sql)) {
         // Bind variables to the prepared statement as parameters
-        $stmt->bind_param("sssss", $FirstName, $LastName, $PhoneNumber, $CarType, $Email);
+        $stmt->bind_param("sssss", $first_name, $last_name, $contact_number, $car_type, $email);
 
         // Execute the statement and check if it was successful
         if ($stmt->execute()) {
