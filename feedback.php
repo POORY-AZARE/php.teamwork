39f0e511d7017be02820f2f81bc27ef25a0106d5
charity <charity23000@student.hamk.fi>
2024-02-25 16:15:35 +0200

    Update feedback.php

--- feedback.php
+++ feedback.php
@@ -7,7 +7,7 @@ include 'header.php'; ?>
 
     <div class="form-group">
 
-        <label form="FullName">FullName</label>
+        <label for="FullName">FullName</label>
         <input type="text" class="form-control" id="FullName" placeholder="Enter your FullName" name="FullName"  required minlength="3" maxlength="20">
 
          <span id="FullNameError"></span>
